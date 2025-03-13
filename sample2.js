175829be6e5a331a6642fb32c7830a5fd947bfc3
jtkdx <jtakedaiwd@hotmail.com>
2025-03-13 11:24:10 +0900

    Update sample2.js

--- sample2.js
+++ sample2.js
@@ -1,11 +1,1 @@
-ObjC.import('Foundation');
-
-var fileManager = $.NSFileManager.defaultManager;
-var path = '/Users/runner/Desktop/testFile.txt'; // 必要に応じて絶対パスに変更
-var success = fileManager.createFileAtPathContentsAttributes(path, null, null);
-
-if (success) {
-    console.log('ファイル作成に成功しました: ' + path);
-} else {
-    console.error('ファイル作成に失敗しました: ' + path);
-}
+console.log("Hello, World!");
